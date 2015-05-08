6cd11e9b761922c5fa820d82b12080a986d4f77e
andy <andrew.glassman@gmail.com>
2015-05-08 16:40:12 -0500

    Added scale generation based on root note, mode, and scale template.

--- src/main/java/com/conchordance/music/Scale.java
+++ src/main/java/com/conchordance/music/Scale.java
@@ -1,39 +1,177 @@
 package com.conchordance.music;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import static com.conchordance.music.NoteName.*;
+import java.util.List;
+import java.util.Map;
+
 
 public class Scale {
 	
-	private static final HashMap<Note, Scale> MAJOR_SCALES;
-
+	private static Map<Integer,List<Note>> CHROMATIC_SCALE;
+	
 	static {
-		MAJOR_SCALES = new HashMap<>();
-		MAJOR_SCALES.put(new Note(C, 0), new Scale(C, new int[]{0,0,0,0,0,0,0}));
-		MAJOR_SCALES.put(new Note(C, 1), new Scale(C, new int[]{1,1,1,1,1,1,1}));
-		MAJOR_SCALES.put(new Note(D, -1), new Scale(D, new int[]{-1,-1,0,-1,-1,-1,0}));
-		MAJOR_SCALES.put(new Note(D, 0), new Scale(D, new int[]{0,0,1,0,0,0,1}));
-		MAJOR_SCALES.put(new Note(D, 1), new Scale(D, new int[]{1,1,2,1,1,1,2}));
-		MAJOR_SCALES.put(new Note(E, -1), new Scale(E, new int[]{-1,0,0,-1,-1,0,0}));
-		MAJOR_SCALES.put(new Note(E, 0), new Scale(E, new int[]{0,1,1,0,0,1,1}));
-		MAJOR_SCALES.put(new Note(F, 0), new Scale(F, new int[]{0,0,0,-1,0,0,0}));
-		MAJOR_SCALES.put(new Note(F, 1), new Scale(F, new int[]{1,1,1,0,1,1,1}));
-		MAJOR_SCALES.put(new Note(G, -1), new Scale(G, new int[]{-1,-1,-1,-1,-1,-1,0}));
-		MAJOR_SCALES.put(new Note(G, 0), new Scale(G, new int[]{0,0,0,0,0,0,1}));
-		MAJOR_SCALES.put(new Note(G, +1), new Scale(G, new int[]{1,1,1,1,1,1,2}));
-		MAJOR_SCALES.put(new Note(A, -1), new Scale(A, new int[]{-1,-1,0,-1,-1,0,0}));
-		MAJOR_SCALES.put(new Note(A, 0), new Scale(A, new int[]{0,0,1,0,0,1,1}));
-		MAJOR_SCALES.put(new Note(A, 1), new Scale(A, new int[]{1,1,2,1,1,2,2}));
-		MAJOR_SCALES.put(new Note(B, -1), new Scale(B, new int[]{-1,0,0,-1,0,0,0}));
-		MAJOR_SCALES.put(new Note(B, 0), new Scale(B, new int[]{0,1,1,0,1,1,1}));
-		MAJOR_SCALES.put(new Note(B, 1), new Scale(B, new int[]{1,2,2,1,2,2,2}));
-		MAJOR_SCALES.put(new Note(C, -1), new Scale(C, new int[]{-1,-1,-1,-1,-1,-1,-1}));
+		CHROMATIC_SCALE = new HashMap<Integer, List<Note>>();
+		CHROMATIC_SCALE.put(0, Arrays.asList(new Note(NoteName.C,0)));
+		CHROMATIC_SCALE.put(1, Arrays.asList(new Note(NoteName.C,1),new Note(NoteName.D,-1)));
+		CHROMATIC_SCALE.put(2, Arrays.asList(new Note(NoteName.D,0)));
+		CHROMATIC_SCALE.put(3, Arrays.asList(new Note(NoteName.D,1),new Note(NoteName.E,-1)));
+		CHROMATIC_SCALE.put(4, Arrays.asList(new Note(NoteName.E,0)));
+		CHROMATIC_SCALE.put(5, Arrays.asList(new Note(NoteName.F,0)));
+		CHROMATIC_SCALE.put(6, Arrays.asList(new Note(NoteName.F,1),new Note(NoteName.G,-1)));
+		CHROMATIC_SCALE.put(7, Arrays.asList(new Note(NoteName.G,0)));
+		CHROMATIC_SCALE.put(8, Arrays.asList(new Note(NoteName.G,1),new Note(NoteName.A,-1)));
+		CHROMATIC_SCALE.put(9, Arrays.asList(new Note(NoteName.A,0)));
+		CHROMATIC_SCALE.put(10, Arrays.asList(new Note(NoteName.A,1),new Note(NoteName.B,-1)));
+		CHROMATIC_SCALE.put(11, Arrays.asList(new Note(NoteName.B,0)));
 	}
+	
+	public enum ScaleTemplate {
+		MAJOR(2,2,1,2,2,2,1),
+		MINOR(2,1,2,2,1,2,2),
+		HARMONIC_MINOR(2,1,2,2,1,3,1);
+		
+		public final int steps;
+		public final int[] stepSequence;
+		
+		ScaleTemplate(int ... stepSequence)
+		{
+			this.steps = stepSequence.length;
+			this.stepSequence = stepSequence;
+		}
+	}
+	
+	public enum Mode {
+		IONIAN(0),
+		DORIAN(1),
+		PHRYGIAN(2),
+		LYDIAN(3),
+		MIXOLYDIAN(4),
+		AEOLIAN(5),
+		LOCRIAN(6);
+		
+		public final int transposition;
+		
+		Mode(int transposition)
+		{
+			this.transposition = transposition;
+		}
+	}
+	
+	
+	
 	
 	public static Scale getMajorScale(Note root) {
-		return MAJOR_SCALES.get(root);
+		return new Scale(root,ScaleTemplate.MAJOR,Mode.IONIAN);
 	}
 	
+	public static Scale getMajorScale(Note root, Mode mode) {
+		return new Scale(root,ScaleTemplate.MAJOR,mode);
+	}
+	
+	public static Scale getRelativeMinor(Note root) {
+		return new Scale(root,ScaleTemplate.MAJOR,Mode.AEOLIAN);
+	}
+	
+	public static Scale getMinorScale(Note root) {
+		return new Scale(root,ScaleTemplate.MINOR,Mode.IONIAN);
+	}
+	
+	public static Scale getMinorScale(Note root, Mode mode) {
+		return new Scale(root,ScaleTemplate.MINOR,mode);
+	}
+	
+	public static Scale getHarmonicMinorScale(Note root) {
+		return new Scale(root,ScaleTemplate.HARMONIC_MINOR,Mode.IONIAN);
+	}
+	
+	public static Scale getHarmonicMinorScale(Note root, Mode mode) {
+		return new Scale(root,ScaleTemplate.HARMONIC_MINOR,mode);
+	}
+	
+	private Scale(Note root, ScaleTemplate scaleTemplate, Mode mode) {
+		
+		Note[] generatedScale = new Note[7];
+		
+		Integer semitones = root.halfSteps;
+		
+		for(int scaleIndex = 0; scaleIndex < scaleTemplate.steps; scaleIndex++) {
+			generatedScale[scaleIndex] = determineNote(root, semitones%12,semitones/12);			
+			semitones += scaleTemplate.stepSequence[scaleIndex];
+		}
+		
+		generatedScale = transpose(generatedScale,mode);
+		
+		notes = generatedScale;
+	}
+
+	/**
+	 * Determine the next note in the scale.
+	 * 
+	 * If root note is flat, all notes with shared names will use flat.
+	 * This is probably incorrect theory wise, as this actually depends on what key the scale is being played in.
+	 * 
+	 * @param root The root note of the scale.
+	 * @param semitones The number of semitones from C
+	 * @param octave  The current octave
+	 * @return
+	 */
+	private Note determineNote(Note root, Integer semitones, Integer octave) {
+		Integer sharedNoteNameIndex = 0;
+		
+		if(root.modifier < 0 && CHROMATIC_SCALE.get(semitones).size() > 1)
+			sharedNoteNameIndex = 1;
+		
+		Note foundNote = CHROMATIC_SCALE.get(semitones).get(sharedNoteNameIndex);
+		Note trueScaleNote = new Note(foundNote.noteName, foundNote.modifier, octave);
+		
+		return trueScaleNote;
+	}
+
+	/**
+	 * Transpose the scaleNotes array by the supplied Mode offset.
+	 * 
+	 * @param scaleNotes
+	 * @param mode
+	 * @return
+	 */
+	private static Note[] transpose(Note[] scaleNotes, Mode mode) {
+		if(mode.transposition > 0) {
+			Note[] transposedScaleNotes = new Note[scaleNotes.length];
+			Note[] subTransposedNotes1 = Arrays.copyOfRange(scaleNotes, mode.transposition, scaleNotes.length);
+			Note[] subTransposedNotes2 = Arrays.copyOfRange(scaleNotes, 0, mode.transposition);
+			System.arraycopy(subTransposedNotes1, 0, transposedScaleNotes, 0, subTransposedNotes1.length);
+			System.arraycopy(subTransposedNotes2, 0, transposedScaleNotes, scaleNotes.length - mode.transposition, subTransposedNotes2.length);
+			return fixOctaves(transposedScaleNotes);
+		}
+		return scaleNotes;
+	}
+	
+	/**
+	 * Due to the way the note class works, octaves are based on C.  When a mode
+	 * is used to transpose a scale, the note octaves are not in the correct order.
+	 * 
+	 * This method will resolve any problems with the note octave after transposition.
+	 * 
+	 * @param modulatedScaleNotes
+	 * @return modulatedScaleNotes with the correct octave values
+	 */
+	private static Note[] fixOctaves(Note[] modulatedScaleNotes) {
+		Note[] fixedOctaveScaleNotes = new Note[modulatedScaleNotes.length];
+		
+		int octave = 0;		
+
+		for(int i = 0; i < modulatedScaleNotes.length; i++)	{	
+			Note currentNote = modulatedScaleNotes[i];
+			
+			if( i > 0 && currentNote.noteName == NoteName.C)
+				octave++;
+			
+			fixedOctaveScaleNotes[i] = new Note(currentNote.noteName,currentNote.modifier,octave);
+		}
+		return fixedOctaveScaleNotes;
+	}
+
 	public String toString() {
 		String str = "";
 		for (int i = 0; i<notes.length-1; ++i)
@@ -56,16 +194,10 @@ public class Scale {
 	public Note getNote(int majorInterval) {
 		return notes[majorInterval - 1];
 	}
-
-	private Scale(NoteName root, int[] modifiers) {
-		notes = new Note[modifiers.length];
-		int octave = 0;
-		for (int i = 0; i<notes.length; ++i) {
-            NoteName note = root.offset(i);
-			if (i > 0 && note == NoteName.C)
-				++octave;
-			notes[i] = new Note(note, modifiers[i], octave);
-		}
+	
+	public Note[] scaleNotes()
+	{
+		return notes.clone();
 	}
 	
 	private Note[] notes;
--- src/test/java/com/conchordance/music/ScaleTest.java
+++ src/test/java/com/conchordance/music/ScaleTest.java
@@ -1,22 +1,113 @@
 package com.conchordance.music;
 
-import org.junit.Test;
-import java.text.MessageFormat;
-import static org.junit.Assert.assertEquals;
 import static com.conchordance.music.NoteName.*;
+import static org.junit.Assert.assertEquals;
+
+import java.text.MessageFormat;
+
+import org.junit.Test;
+
+import com.conchordance.music.Scale.Mode;
 
 
 public class ScaleTest {
 
-	@Test
-	public void testGetMajorScale() {
-		String cScaleString = Scale.getMajorScale(new Note(C, 0)).toString();
-		assertEquals("C Scale", "C0, D0, E0, F0, G0, A0, B0", cScaleString);
-
-		String bScaleString = Scale.getMajorScale(new Note(B, 0)).toString();
-		String bScaleManual = MessageFormat.format("B0, C{0}1, D{0}1, E1, F{0}1, G{0}1, A{0}1", Note.SHARP);
-		assertEquals("B Scale", bScaleManual, bScaleString);
+	@Test 
+	public void testGetAMajorScale(){
+		String scaleName = "A Major Scale";
+		String expected = MessageFormat.format("A0, B0, C{0}1, D1, E1, F{0}1, G{0}1", Note.SHARP);
+		String actual = Scale.getMajorScale(new Note(A, 0)).toString();
+		assertEquals(scaleName, expected, actual);
 	}
+	
+	@Test 
+	public void testGetAMajorScale_Dorian(){
+		String scaleName = "A Major Scale - Dorian Mode";
+		String expected = MessageFormat.format("B0, C{0}1, D1, E1, F{0}1, G{0}1, A1", Note.SHARP);
+		String actual = Scale.getMajorScale(new Note(A, 0),Mode.DORIAN).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+	
+	@Test 
+	public void testGetAMajorScale_Phrygian(){
+		String scaleName = "A Major Scale - Phrygian Mode";
+		String expected = MessageFormat.format("C{0}0, D0, E0, F{0}0, G{0}0, A0, B0", Note.SHARP);
+		String actual = Scale.getMajorScale(new Note(A, 0),Mode.PHRYGIAN).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+	
+	@Test 
+	public void testGetAMajorScale_Lydian(){
+		String scaleName = "A Major Scale - Lydian Mode";
+		String expected = MessageFormat.format("D0, E0, F{0}0, G{0}0, A0, B0, C{0}1", Note.SHARP);
+		String actual = Scale.getMajorScale(new Note(A, 0),Mode.LYDIAN).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+	
+	@Test 
+	public void testGetAMajorScale_Mixolydian(){
+		String scaleName = "A Major Scale - Mixolydian Mode";
+		String expected = MessageFormat.format("E0, F{0}0, G{0}0, A0, B0, C{0}1, D1", Note.SHARP);
+		String actual = Scale.getMajorScale(new Note(A, 0),Mode.MIXOLYDIAN).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+	
+	@Test 
+	public void testGetAMajorScale_Aeolian(){
+		String scaleName = "A Major Scale - Aeolian Mode";
+		String expected = MessageFormat.format("F{0}0, G{0}0, A0, B0, C{0}1, D1, E1", Note.SHARP);
+		String actual = Scale.getMajorScale(new Note(A, 0),Mode.AEOLIAN).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+	
+	@Test 
+	public void testGetAMajorScale_Locrian(){
+		String scaleName = "A Major Scale - Locrian Mode";
+		String expected = MessageFormat.format("G{0}0, A0, B0, C{0}1, D1, E1, F{0}1", Note.SHARP);
+		String actual = Scale.getMajorScale(new Note(A, 0),Mode.LOCRIAN).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+	
+	@Test 
+	public void testGetASharpMajorScale(){
+		String scaleName = "A Sharp Major Scale";
+		String expected = MessageFormat.format("A{0}0, C1, D1, D{0}1, F1, G1, A1", Note.SHARP);
+		String actual = Scale.getMajorScale(new Note(A, 1)).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+	
+	@Test 
+	public void testGetAFlatMajorScale(){
+		String scaleName = "A Flat Major Scale";
+		String expected = MessageFormat.format("A{0}0, B{0}0, C1, D{0}1, E{0}1, F1, G1", Note.FLAT);
+		String actual = Scale.getMajorScale(new Note(A, -1)).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+	
+	@Test
+	public void testGetCMajorScale() {
+		String scaleName = "C Major Scale";
+		String expected = "C0, D0, E0, F0, G0, A0, B0";
+		String actual = Scale.getMajorScale(new Note(C, 0)).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+	
+	@Test
+	public void testGetCMinorScale() {
+		String scaleName = "C Minor Scale";
+		String expected = MessageFormat.format("C0, D0, D{0}0, F0, G0, G{0}0, A{0}0", Note.SHARP);
+		String actual = Scale.getMinorScale(new Note(C,0)).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+	
+	@Test
+	public void testGetBMajorScale() {
+		String scaleName = "B Major Scale";
+		String expected = MessageFormat.format("B0, C{0}1, D{0}1, E1, F{0}1, G{0}1, A{0}1", Note.SHARP);
+		String actual = Scale.getMajorScale(new Note(B, 0)).toString();
+		assertEquals(scaleName, expected, actual);
+	}
+
 
 	@Test
 	public void testApplyIntervals() {
